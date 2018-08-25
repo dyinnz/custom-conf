4826da0312480bafc79977835ff3926e9e242d83
Guo Yingzhong <guoyingzhong@bytedance.com>
2018-08-25 23:21:52 +0800

    update common-env and zshrc

--- common-env.sh
+++ common-env.sh
@@ -1,16 +1,19 @@
 export TERM=screen-256color
 
-export C_INCLUDE_PATH=$HOME/local/include:$C_INCLUDE_PATH
-export CPLUS_INCLUDE_PATH=$HOME/local/include:$CPLUS_INCLUDE_PATH
+if [ "$(uname -s)" == "Linux" ]; then
+    export LOCAL_PATH=$HOME/local
+else
+    export LOCAL_PATH=$HOME/Local
+fi
 
-export LIBRARY_PATH=$HOME/local/lib:$LIBRARY_PATH
-export LIBRARY_PATH=$HOME/local/lib64:$LIBRARY_PATH
-export LD_LIBRARY_PATH=$HOME/local/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$HOME/local/lib64:$LD_LIBRARY_PATH
+export C_INCLUDE_PATH=$LOCAL_PATH/include:$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=$LOCAL_PATH/include:$CPLUS_INCLUDE_PATH
 
-export PATH=$HOME/local/bin:$PATH
+export LIBRARY_PATH=$LOCAL_PATH/lib:$LIBRARY_PATH
+export LIBRARY_PATH=$LOCAL_PATH/lib64:$LIBRARY_PATH
+export LD_LIBRARY_PATH=$LOCAL_PATH/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$LOCAL_PATH/lib64:$LD_LIBRARY_PATH
 
-export PATH=$HOME/local/nvim/bin:$PATH
+export PATH=$LOCAL_PATH/bin:$PATH
 
 alias agcpp="ag --cpp"
-
--- zshrc
+++ zshrc
@@ -1,7 +1,7 @@
 # oh my zsh
 export ZSH=$HOME/.oh-my-zsh
 
-ZSH_THEME="bureau"
+ZSH_THEME="bira"
 
 plugins=(
   git
@@ -10,7 +10,8 @@ plugins=(
 
 source $ZSH/oh-my-zsh.sh
 
-# custom
+# common
 source $HOME/.custom-conf/common-env.sh
+source $HOME/.custom-conf/custom-env.sh
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
