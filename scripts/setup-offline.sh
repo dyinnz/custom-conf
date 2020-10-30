fa5de22071049a782ee05e896b90c90207a06a0b
Guo Yingzhong <guoyingzhong@bytedance.com>
2020-10-30 11:05:03 +0800

    update setup-offline

--- scripts/setup-offline.sh
+++ scripts/setup-offline.sh
@@ -3,14 +3,25 @@ set -x
 
 CURR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 ROOT="$(dirname $CURR)"
-echo $ROOT
+cd .
 
 ln -s $HOME/.local/share/nvim/site/autoload/plug.vim $HOME/package/plug.vim
 
-unzip $ROOT/package/ycm.zip -d $HOME/.vim/
+unzip ./package/ycm.zip -d $HOME/.vim/
 
-unzip $ROOT/package/oh-my-zsh.zip -d $ROOT
-mv $ROOT/ohmy-zsh-master $HOME/.oh-my-zsh
+unzip ./package/oh-my-zsh.zip
+mv ./ohmyzsh-master $HOME/.oh-my-zsh
 
-unzip $ROOT/package/zsh-autosuggestions.zip -d $ROOT
-mv $ROOT/zsh-autosuggestions-master $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
+unzip ./package/zsh-autosuggestions.zip
+mv ./zsh-autosuggestions-master $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
+
+
+# $HOME/local
+mkdir -p $HOME/local
+
+tar zxf package/nvim.tar.gz -C $HOME/local
+mv $HOME/local/nvim-linux64 $HOME/local/nvim
+
+chmod +x package/tmux.AppImage
+package/tmux.AppImage --appimage-extract
+mv squashfs-root $HOME/local/tmux-app
